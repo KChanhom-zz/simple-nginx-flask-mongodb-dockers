94cbf147838d3502b8e19c72b8928afa47d7cb54
KChanhomm <kiettisak.chan@gmail.com>
2020-12-02 15:55:12 -0500

    First commit

---
+++ pricing_data/app/app.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask
+from pymongo import MongoClient
+from random import randint
+
+app = Flask(__name__)
+username = os.environ.get("MONGO_INITDB_ROOT_USERNAME")
+password = os.environ.get("MONGO_INITDB_ROOT_PASSWORD")
+client = MongoClient(
+    "mongodb://mongodb:27017",
+    username=username,
+    password=password
+)
+db = client['db']
+
+@app.route('/test')
+def hello():
+    return 'Hello World!'
+
+
+@app.route('/api')
+def insert_mongo_db():
+    rand = randint(0, 100)
+    item = {'number': randint(0, 100)}
+    db['number'].insert_one(item)
+    return 'Insert ' + str(rand) + ' into MongoDB!'
+
+@app.route('/api/get')
+def get_mongo_db():
+    rand = db['number'].find_one()['number']
+    return 'Get ' + str(rand) + ' from MongoDB!'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
